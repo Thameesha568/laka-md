1ae5e9358ed6cd139025ee0dd306553284b03352
Thameesha568 <thameeshavishal568@gmail.com>
2025-03-08 12:20:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "session id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=olJznC6L#FXt9TtEEtKvg7T0ioG5_0AeykMAYt7CU9kPmuJFvbT0",
 PREFIX: process.env.PREFIX || '.' ,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
